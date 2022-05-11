25669b6559fc2c0f15045fa57b132b9fa9ccbaf3
Madhan-Offi <102455806+Madhan-Offi@users.noreply.github.com>
2022-05-11 18:40:18 +0530

    Main

--- main.py
+++ main.py
@@ -56,6 +56,33 @@ async def gplink(_, message):
     await message.reply_text(f"{gpLink}")
 
 
+def droplink_bypass(url):
+    client = requests.Session()
+    res = client.get(url)
 
-bot.run() 
+    ref = re.findall("action[ ]{0,}=[ ]{0,}['|\"](.*?)['|\"]", res.text)[0]
+
+    h = {'referer': ref}
+    res = client.get(url, headers=h)
+
+    bs4 = BeautifulSoup(res.content, 'lxml')
+    inputs = bs4.find_all('input')
+    data = { input.get('name'): input.get('value') for input in inputs }
+
+    h = {
+        'content-type': 'application/x-www-form-urlencoded',                        
+        'x-requested-with': 'XMLHttpRequest'
+    }
+    p = urlparse(url)
+    final_url = f'{p.scheme}://{p.netloc}/links/go'
+
+    time.sleep(3.1)
+    res = client.post(final_url, data=data, headers=h).json()
+
+    return res
+
+
+
+bot.run()
+ 
 
